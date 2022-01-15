bd217f8653eb438dc0ccbbafa1c9a24960f29c6e
Omar Alhaddar <omaralhaddar11@gmail.com>
2022-01-15 16:21:53 +0200

    register auth

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./config.env" });
+
+const cookieParser = require('cookie-parser');
+
 require("./server/config/mongoose.config");
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 require("./server/routes/post.route")(app);
@@ -12,3 +18,6 @@ require("./server/routes/user.route")(app);
 const server = app.listen(8000, () => {
   console.log("Listening at Port 8000");
 });
+
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,24 +1,55 @@
-const Post = require("../models/post.model");
 const User = require("../models/user.model");
 
-module.exports.register = (req, res) => {
-  User.create(req.body)
-    .then((user) => {
-      const userToken = jwt.sign(
-        {
-          id: user._id,
-        },
-        process.env.SECRET_KEY
-      );
 
-      res
-        .cookie("usertoken", userToken, secret, {
-          httpOnly: true,
-        })
-        .json({ msg: "success!", user: user });
-    })
-    .catch((err) => res.json(err));
+exports.register = async (req, res, next) => {
+  const { username, email, password, countrycode, phonenumber  } = req.body;
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+      countrycode,
+      phonenumber,
+
+    });
+
+    sendToken(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
 };
+
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check if email and password is provided
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  try {
+    // Check that user exists by email
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Check that password match
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    sendToken(user, 201, res);
+  } catch (err) {
+    next(err);
+  }
+};
+
 
 module.exports.all = (req, res) => {
   User.find()
@@ -33,35 +64,6 @@ module.exports.findOneUser = (req, res) => {
     .catch((err) => res.json({ message: "Something went wrong", error: err }));
 };
 
-module.exports.login = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  if (user === null) {
-    return res.sendStatus(400);
-  }
-
-  const correctPassword = await bcrypt.compare(
-    req.body.password,
-    user.password
-  );
-
-  if (!correctPassword) {
-    return res.sendStatus(400);
-  }
-
-  const userToken = jwt.sign(
-    {
-      id: user._id,
-    },
-    process.env.SECRET_KEY
-  );
-
-  res
-    .cookie("usertoken", userToken, secret, {
-      httpOnly: true,
-    })
-    .json({ msg: "success!" });
-};
 
 module.exports.deleteuser = (request, response) => {
   User.deleteOne({ _id: request.params.id })
@@ -73,4 +75,10 @@ module.exports.logout = (req, res) => {
   res.clearCookie("usertoken");
   res.json({ msg: "User Logged Out" });
   res.sendStatus(200);
+};
+
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+  res.status(statusCode).json({ sucess: true, token });
 };
